7ad040fd743336d45c3dcecb40ecbfeaa3dc05a5
Jsaikia47 <54184823+Jsaikia47@users.noreply.github.com>
2021-02-04 00:21:44 -0700

    Delete BinaryModel directory

--- BinaryModel/models/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from .alexnet import *
-from .alexnet_binary import *
-from .resnet import *
-from .resnet_binary import *
-from .resnet_binary_act_quant import *
-from .resnet_binary_act_quant1 import *
-from .vgg_cifar10_binary import *
--- BinaryModel/models/quant_modules/__init__.py
+++
@@ -1,4 +0,0 @@
-from .quantizer import *
-from .quant_layer import *
-from .quant_dorefa import *
-from .pact_w import *
